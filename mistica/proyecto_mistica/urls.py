dd5900a4945038f493071c1bd18b9bcefe116132
Jessica Battistino <batt.learning@gmail.com>
2024-12-09 15:30:36 -0300

    rename project

--- mistica/proyecto_mistica/urls.py
+++ mistica/proyecto_mistica/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for mistica project.
+URL configuration for proyecto_mistica project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
